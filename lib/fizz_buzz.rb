67497b1edb9c0b6bdb71b0e1924f2524ce9d843c
Dillon Hafer <dh@dillonhafer.com>
2014-10-10 10:53:06 -0400

    Updates internal functions to avoid use of modulus 15

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,37 +1,35 @@
 class FizzBuzz
+  DEFINITIONS = {'3' => 'fizz', '5' => 'buzz'}
   attr_accessor :number  
 
   def initialize(number)
     @number = number
   end
 
-  def word
-    if divisible?
-      definitions[find]
-    else 
-      number
-    end
-  end
-
   def valid?
     number > 0
   end
+  
+  def to_s
+    convert if valid?
+  end
 
   private
-
-  def divisible?
-    valid? && !definitions[find].nil?
-  end
-
-  def definitions
-    {'3' => 'fizz', '5' => 'buzz', '15' => 'fizzbuzz'}
-  end
-
-  def find
-    match = number
-    %w{3 5 15}.each do |n|
-      match = n if number % n.to_i == 0
+    
+  def calculate
+    match = ""
+    %w{3 5}.each do |n|
+      match << DEFINITIONS[n] if number % n.to_i == 0
     end
     match
+  end
+
+  def convert
+    result = calculate
+    if result.empty?
+      number
+    else
+      result
+    end
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -13,44 +13,44 @@ describe FizzBuzz, "#valid?" do
   end
 end
 
-describe FizzBuzz, "#word" do
+describe FizzBuzz, "#to_s" do
   it "returns 1 if 1 not divisible by 3, 5, or 15" do
     fizz_buzz = FizzBuzz.new(1)
-    expect(fizz_buzz.word).to eq(1)
+    expect(fizz_buzz.to_s).to eq(1)
   end
 
   it "returns 2 if 2 not divisible by 3, 5, or 15" do
     fizz_buzz = FizzBuzz.new(2)
-    expect(fizz_buzz.word).to eq(2)
+    expect(fizz_buzz.to_s).to eq(2)
   end
   
   it "returns 'fizz' if the number is divisible by 3" do
     fizz_buzz = FizzBuzz.new(3)
-    expect(fizz_buzz.word).to eq('fizz')
+    expect(fizz_buzz.to_s).to eq('fizz')
   end
 
   it "returns 4 if 4 is not divisible by 3, 5, or 15" do
     fizz_buzz = FizzBuzz.new(4)
-    expect(fizz_buzz.word).to eq(4)
+    expect(fizz_buzz.to_s).to eq(4)
   end
   
   it "returns 'buzz' if the number is divisible by 5" do    
     fizz_buzz = FizzBuzz.new(5)
-    expect(fizz_buzz.word).to eq('buzz')
+    expect(fizz_buzz.to_s).to eq('buzz')
   end
 
   it "returns 'fixx' if the number is divisible by 3" do
     fizz_buzz = FizzBuzz.new(6)
-    expect(fizz_buzz.word).to eq('fizz')
+    expect(fizz_buzz.to_s).to eq('fizz')
   end
 
   it "returns 'buzz' if the number is divisible by 5" do    
     fizz_buzz = FizzBuzz.new(10)
-    expect(fizz_buzz.word).to eq('buzz')
+    expect(fizz_buzz.to_s).to eq('buzz')
   end
 
   it "returns 'fizzbuzz' if the number is divisible by 15" do
     fizz_buzz = FizzBuzz.new(15)
-    expect(fizz_buzz.word).to eq('fizzbuzz')
+    expect(fizz_buzz.to_s).to eq('fizzbuzz')
   end  
 end
